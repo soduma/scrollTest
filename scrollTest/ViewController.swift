26f6e468085221d4e647462edea83f740e92f454
soduma <soduma@naver.com>
2022-01-17 12:25:56 +0900

    Initial Commit

---
+++ scrollTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scrollTest
+//
+//  Created by 장기화 on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
